b828f0960fc114c74c74a136273d5908ad98950c
mohankumar2698 <135440495+mohankumar2698@users.noreply.github.com>
2026-06-08 23:40:36 +0530

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,18 @@
-print('clean')
+import pandas as pd
+
+df = pd.read_csv(
+    "../data/Global_Climate_Energy_Transition_Project_2000_2026.csv"
+)
+
+print(df.info())
+
+df.drop_duplicates(inplace=True)
+
+df.fillna(method="ffill", inplace=True)
+
+df.to_csv(
+    "../data/climate_energy_cleaned.csv",
+    index=False
+)
+
+print("Cleaning Complete")
